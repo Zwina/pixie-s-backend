7e9afa5e7554c62181dcc9710d38dfc907a645e6
Marjorie Kehil <marjorie.kehil28@hotmail.com>
2023-10-11 14:27:12 +0200

    chgmt bd

--- config/db.js
+++ config/db.js
@@ -3,11 +3,14 @@ const mysql = require('mysql')
 
 const db = mysql.createConnection({
 // host: "localhost",
+// host: "sql11.freesqldatabase.com",
 host: "sql11.freesqldatabase.com",
 // user: "root",
-user: "sql11649784",
+// user: "sql11649784",
+user: "sql11652518",
 // password: "",
-password: "3KHbZMaqBG",
+// password: "3KHbZMaqBG",
+password: "MZwfUTYAvK",
 database: process.env.URL_DATABASE
 })
 
